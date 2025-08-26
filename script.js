59db0b426e9f019c4c3beb281f96b91c18089269
yash25a <36102734+yash25a@users.noreply.github.com>
2025-08-26 14:41:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+// Dark Mode Toggle
+const toggleBtn = document.getElementById("dark-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  toggleBtn.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
+});
